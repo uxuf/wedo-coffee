8ed5279c678a8261158395260638e55b20ad0ee3
Yousuf Shaikh <yousuf.shaikh@cbsi.com>
2013-09-18 01:54:09 +0100

    Setup bundle

---
+++ src/Wedo/GrinderBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wedo\GrinderBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('WedoGrinderBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Wedo/GrinderBundle/WedoGrinderBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wedo\GrinderBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WedoGrinderBundle extends Bundle
+{
+}
