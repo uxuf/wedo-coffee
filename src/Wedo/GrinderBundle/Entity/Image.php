846f0b22cc4fee1bcdba94fc2c1f01f063b2392d
Yousuf Shaikh <yousuf.shaikh@cbsi.com>
2013-09-22 21:00:43 +0100

    Add db entities

---
+++ src/Wedo/GrinderBundle/Entity/Image.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Wedo\GrinderBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Image
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Wedo\GrinderBundle\Entity\ImageRepository")
+ */
+class Image
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="link", type="string", length=500)
+     */
+    private $link;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set link
+     *
+     * @param string $link
+     * @return Image
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    /**
+     * Get link
+     *
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+}
